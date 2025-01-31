091d99dbc0b9d8e77d17cf0f228342aff87e3059
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-01-31 14:51:19 +0530

    login page added

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Loginpage from './pages/login.jsx'
 import App from './App.jsx'
+import Navbar from './pages/NAvbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    
   </StrictMode>,
 )
---
+++ FRONTEND/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaBeer } from 'react-icons/fa';
+
+
+function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      
+      <h1 className="text-6xl font-semibold">Welcome to Home</h1>
+    </div>
+  );
+}
+
+export default Home;
